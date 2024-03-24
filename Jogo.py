dc307a459d49626200df8587c90e1686b2d7757d
João Prado <joao.victor0705@gmail.com>
2024-03-23 22:29:32 -0300

    Part of game and the main functions

--- Jogo.py
+++ Jogo.py
@@ -3,8 +3,30 @@ import time
 
 jogador1 = input("Nome do jogador 1: ")
 jogador2 = input("Nome do jogador 2: ")
+time.sleep(0.5)
+print("Bem vindos jogadores,",jogador1, "e", jogador2)
 
-fc.Media()
+tabuleiro = [" "]*10
+jogadores = [jogador1, jogador2]
+jogador_atual = 0
 
-print("Bora começar o jogo!!!!")
-time.sleep(2)
+fc.formato_velha(tabuleiro)
+posicao = int(input(f"Jogador(a) {jogadores[jogador_atual]}, escolha uma posição de 1 a 9: "))
+
+while True:
+    if tabuleiro[posicao] != "":        
+        print("Essa posição já está ocupada.")
+        continue
+    
+    tabuleiro[posicao] = jogadores[jogador_atual]
+    
+    if fc.checar_vencedor(tabuleiro, jogadores[jogador_atual]):
+        fc.formato_velha(tabuleiro)
+        print(f"Jogador(a) {jogadores[jogador_atual]} venceu!")
+        break
+    if all(celula != " " for celula in tabuleiro):
+        fc.formato_velha(tabuleiro)
+        print("Empate!")
+        break
+    
+    jogador_atual = (jogador_atual + 1) % 2
